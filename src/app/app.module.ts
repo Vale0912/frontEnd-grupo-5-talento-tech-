a50103bba93271a1e21c77c7dac7eec27ec1a366
Jonathan Suárez <Jotha298@users.noreply.github.com>
2024-12-03 17:00:42 -0500

    Se organiza logo en footer y header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,14 @@ import { EmprendedorComponent } from './components/emprendedor/emprendedor.compo
 import { CreateUserComponent } from './components/create-user/create-user.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
+
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { VerDetalleComponent } from './components/ver-detalle/ver-detalle.component';
 import { FooterComponent } from './shared/footer/footer.component';
-=======
+
 import { HeaderComponent } from './components/header/header.component';
 import { CreateEmprendimientoComponent } from './components/create-emprendimiento/create-emprendimiento.component';
->>>>>>> a5404c6734a35c024dbdf8a36fb9f0058d13ce09
+
 
 @NgModule({
   declarations: [
@@ -23,14 +23,14 @@ import { CreateEmprendimientoComponent } from './components/create-emprendimient
     EmprendedorComponent,
     CreateUserComponent,
     HomeComponent,
-<<<<<<< HEAD
+
     NavbarComponent,
     VerDetalleComponent,
     FooterComponent,
-=======
+
     HeaderComponent,
     CreateEmprendimientoComponent,
->>>>>>> a5404c6734a35c024dbdf8a36fb9f0058d13ce09
+
   ],
   imports: [
     BrowserModule,
